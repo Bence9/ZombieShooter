0d41be89a60fc4d0a1676e073f09615a05dca5e9
Nagy Bence (WH8L7E) <90764142+Bence9@users.noreply.github.com>
2024-03-08 12:20:50 +0100

    Add files via upload

---
+++ Bullet.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace ShooterGame
+{
+    class Bullet
+    {
+        public string direction;
+        public int bulletLeft;
+        public int bulletTop;
+
+        private int speed = 20;
+        private PictureBox bullet = new PictureBox();
+        private Timer bulletTimer = new Timer();
+
+        public void MakeBullet(Form form)
+        {
+            bullet.BackColor = Color.Red;
+            bullet.Size = new Size(5, 5);
+            bullet.Tag = "bullet";
+            bullet.Left = bulletLeft;
+            bullet.Top = bulletTop;
+            bullet.BringToFront();
+
+            form.Controls.Add(bullet);
+
+            bulletTimer.Interval = speed;
+            bulletTimer.Tick += new EventHandler(BulletTimerEvent);
+            bulletTimer.Start();
+
+        }
+
+        private void BulletTimerEvent(object sender, EventArgs e)
+        {
+            if(direction == "left")
+            {
+                bullet.Left -= speed;
+            }
+
+            if (direction == "right")
+            {
+                bullet.Left += speed;
+            }
+
+            if (direction == "up")
+            {
+                bullet.Top -= speed;
+            }
+
+            if (direction == "down")
+            {
+                bullet.Top += speed;
+            }
+
+
+            if (bullet.Left < 10 || bullet.Left > 1300 || bullet.Top < 15 || bullet.Top > 800)
+            {
+                bulletTimer.Stop();
+                bulletTimer.Dispose();
+                bullet.Dispose();
+                bulletTimer = null;
+                bullet = null;
+            }
+
+
+        } 
+
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace ShooterGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtAmmo = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtHealth = new System.Windows.Forms.Label();
+            this.healthBar = new System.Windows.Forms.ProgressBar();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.lbRestart = new System.Windows.Forms.Label();
+            this.lbScore = new System.Windows.Forms.Label();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAmmo
+            // 
+            this.txtAmmo.AutoSize = true;
+            this.txtAmmo.Font = new System.Drawing.Font("Franklin Gothic Demi", 16.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtAmmo.ForeColor = System.Drawing.Color.White;
+            this.txtAmmo.Location = new System.Drawing.Point(12, 12);
+            this.txtAmmo.Name = "txtAmmo";
+            this.txtAmmo.Size = new System.Drawing.Size(130, 36);
+            this.txtAmmo.TabIndex = 0;
+            this.txtAmmo.Text = "Ammo: 0";
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Franklin Gothic Demi", 16.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtScore.ForeColor = System.Drawing.Color.White;
+            this.txtScore.Location = new System.Drawing.Point(312, 12);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(105, 36);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "Kills: 0";
+            // 
+            // txtHealth
+            // 
+            this.txtHealth.AutoSize = true;
+            this.txtHealth.Font = new System.Drawing.Font("Franklin Gothic Demi", 16.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtHealth.ForeColor = System.Drawing.Color.White;
+            this.txtHealth.Location = new System.Drawing.Point(1102, 12);
+            this.txtHealth.Name = "txtHealth";
+            this.txtHealth.Size = new System.Drawing.Size(109, 36);
+            this.txtHealth.TabIndex = 2;
+            this.txtHealth.Text = "Health:";
+            // 
+            // healthBar
+            // 
+            this.healthBar.BackColor = System.Drawing.Color.Gray;
+            this.healthBar.Location = new System.Drawing.Point(1217, 12);
+            this.healthBar.Name = "healthBar";
+            this.healthBar.Size = new System.Drawing.Size(253, 29);
+            this.healthBar.TabIndex = 3;
+            this.healthBar.Value = 100;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::ShooterGame.Properties.Resources.up;
+            this.player.Location = new System.Drawing.Point(754, 452);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(71, 100);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 4;
+            this.player.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // lbRestart
+            // 
+            this.lbRestart.AutoSize = true;
+            this.lbRestart.BackColor = System.Drawing.Color.Transparent;
+            this.lbRestart.Font = new System.Drawing.Font("Franklin Gothic Demi", 26.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbRestart.ForeColor = System.Drawing.Color.Transparent;
+            this.lbRestart.Location = new System.Drawing.Point(475, 300);
+            this.lbRestart.Name = "lbRestart";
+            this.lbRestart.Size = new System.Drawing.Size(606, 51);
+            this.lbRestart.TabIndex = 5;
+            this.lbRestart.Text = "Press ENTER to restart game";
+            this.lbRestart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbRestart.UseWaitCursor = true;
+            this.lbRestart.Visible = false;
+            // 
+            // lbScore
+            // 
+            this.lbScore.AutoSize = true;
+            this.lbScore.Font = new System.Drawing.Font("Franklin Gothic Demi", 16.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbScore.ForeColor = System.Drawing.Color.White;
+            this.lbScore.Location = new System.Drawing.Point(588, 12);
+            this.lbScore.Name = "lbScore";
+            this.lbScore.Size = new System.Drawing.Size(123, 36);
+            this.lbScore.TabIndex = 6;
+            this.lbScore.Text = "Score: 0";
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Font = new System.Drawing.Font("Franklin Gothic Demi", 16.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbTime.ForeColor = System.Drawing.Color.White;
+            this.lbTime.Location = new System.Drawing.Point(856, 12);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(170, 36);
+            this.lbTime.TabIndex = 7;
+            this.lbTime.Text = "Time: 00:00";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1482, 953);
+            this.Controls.Add(this.lbTime);
+            this.Controls.Add(this.lbScore);
+            this.Controls.Add(this.lbRestart);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.healthBar);
+            this.Controls.Add(this.txtHealth);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.txtAmmo);
+            this.Name = "Form1";
+            this.Text = "Zombie Shooter";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtAmmo;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtHealth;
+        private System.Windows.Forms.ProgressBar healthBar;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label lbRestart;
+        private System.Windows.Forms.Label lbScore;
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
